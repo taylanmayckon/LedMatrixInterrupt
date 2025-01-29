bfcc745ab812af84919e23d1792770cb64495408
Taylan <eusoutaylann@gmail.com>
2025-01-29 03:58:38 -0300

    Add: Blink do LED Vermelho 5x por segundo

--- LedMatrixInterrupt.c
+++ LedMatrixInterrupt.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+// Definição dos pinos do Led RGB
+#define RGB_RED 13
+#define RGB_GREEN 11
+#define RGB_BLUE 12
+
+// Definindo a máscara para GPIOs do Led RGB (11, 12 e 13)
+#define GPIO_MASK ((1 << RGB_RED) | (1 << RGB_GREEN) | (1 << RGB_BLUE))
 
 
-int main()
-{
+int main(){
     stdio_init_all();
 
+    // Inicializando todos os pinos da máscara
+    gpio_init_mask(GPIO_MASK);
+    // Definindo como saída
+    gpio_set_dir_out_masked(GPIO_MASK);
+
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        gpio_put(RGB_RED, 1);
+        sleep_ms(100);
+
+        gpio_put(RGB_RED, 0);
+        sleep_ms(100);
     }
 }
